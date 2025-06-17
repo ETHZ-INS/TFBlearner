7f51bc1e8b4f31373b99c06f3e1b3599132a45cc
emsonder <emanuel.sonder@hest.ethz.ch>
2025-06-17 18:02:50 +0200

    Tests for prediction

---
+++ tests/testthat/test-predictTfBinding.R
@@ -0,0 +1,31 @@
+test_that("Predictions: Basic setup",{
+  preds <- NULL
+  expect_no_error(preds <- predictTfBinding(modTest, fmTest, sparsify=FALSE))
+  expect_true(is(preds, "RangedSummarizedExperiment"))
+
+  rangesObs <- rowRanges(preds)
+  rangesObs <- rangesObs[order(rangesObs)]
+
+  rangesExp <- rowRanges(fmTest)
+  rangesExp <- rangesExp[rangesExp$context==levels(rangesExp$context)[[1]]]
+  rangesExp <- rangesExp[order(rangesExp)]
+
+  expect_equal(rangesObs, rangesExp)
+  expect_contains(names(assays(preds)),paste(predPrefix, modelNames, sep="_"))
+  expect_equal(colnames(preds), levels(rangesExp$context))
+
+  fmTest2 <- fmTest
+  metadata(fmTest2)[[tfNameCol]] <- "YY1"
+  expect_error(predictTfBinding(modTest, fmTest2, sparsify=FALSE))
+})
+
+test_that("Predictions: sparsification",{
+  preds <- NULL
+  expect_no_error(preds <- predictTfBinding(modTest, fmTest, sparsify=TRUE))
+})
+
+
+test_that("Predictions: chunking",{
+  preds <- NULL
+  expect_no_error(preds <- predictTfBinding(modTest, fmTest, chunk=10))
+})
